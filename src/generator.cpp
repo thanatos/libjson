e7f3bcecea830640953a68999f1b87a03c800f19
Roy Wellington Ⅳ <cactus_hugged@yahoo.com>
2009-10-22 16:57:08 -0400

    Changes to array's iterator
    ...to make it return Value & instead of Value *.

--- src/generator.cpp
+++ src/generator.cpp
@@ -122,12 +122,12 @@ void json::GeneratorState::loopArray()
 		outputIndent();
 
 	// Get the sub-object
-	const Value *v = *(m_stack.top().i_array);
+	const Value &v = *(m_stack.top().i_array);
 	// Increment now, before we handle the object.
 	// Calling handleItem() might change our stack, so we've got to do this this way.
 	++(m_stack.top().i_array);
 	// And last, do it:
-	handleItem(v);
+	handleItem(&v);
 }
 
 void json::GeneratorState::outputIndent()
